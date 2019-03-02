285876ccada642970e84d504c038fb73a0e1c589
Fabian Wiesmann <fabian.wiesmann@outlook.de>
2019-03-02 19:05:22 +0100

    Actually opens a stuck window

--- SelfDrivingCar/SelfDrivingCar/main.cpp
+++ SelfDrivingCar/SelfDrivingCar/main.cpp
@@ -1,2 +1,29 @@
 #include "Source/OpenGL/glew.h"
 #include "Source/OpenGL/glfw3.h"
+#include <iostream>
+
+void glfw_error_callback (int error, const char* description)
+{
+	std::cout << "(glfw) ERROR: " << description << std::endl;
+}
+
+int main ()
+{
+	if (!glfwInit ())
+	{
+		std::cout << "(glfw) ERROR: glfwInit failed for some reason." << std::endl;
+	}
+
+	glfwSetErrorCallback (glfw_error_callback);
+
+	GLFWwindow* window = glfwCreateWindow (1536, 864, "Back on Track", NULL, NULL);
+	if (!window)
+	{
+		std::cout << "(glfw) ERROR: glfwCreateWindow failed for some reason." << std::endl;
+	}
+
+	while (1)
+	{
+
+	}
+}
